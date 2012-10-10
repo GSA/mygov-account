c4c1270951ca6f4a528af7b148a98ab86509570f
Greg Gershman <greg@shelrick.com>
2012-10-10 14:35:00 -0400

    Fix welcome email to use url helper in view, not in controller, to properly use host information.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @sign_in_url = sign_in_url
     mail(:to => user.email, :subject => 'Welcome to MyGov!')
   end
 end

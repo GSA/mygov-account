3a2f904b94d097811982869cb585a2a8df80d57c
Greg Gershman <greg@shelrick.com>
2012-10-30 01:39:50 -0400

    [Completes #38038669] User sees notice indicating they have no messages

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -17,51 +17,61 @@ describe "Messages" do
     @app2.oauth2_client_owner_id = @user.id
     @app2.save!
     create_logged_in_user(@user)
-    1.upto(14) do |index|
-      @message = Message.new(:subject => "Message ##{index}", :received_at => Time.now - 1.hour, :body => "This is message ##{index}.")
-      @message.user_id = @user.id
-      @message.o_auth2_model_client_id = @app1.id
-      @message.save!
-    end
-    @other_user_message = Message.new(:subject => 'Other User Message', :received_at => Time.now - 1.hour, :body => 'This is a message for a different user.')
-    @other_user_message.user_id = @other_user.id
-    @other_user_message.o_auth2_model_client_id = @app1.id
-    @other_app_message = Message.new(:subject => 'Other App Message', :received_at => Time.now - 1.hour, :body => 'This is a message for a different app.')
-    @other_app_message.user_id = @user.id
-    @other_app_message.o_auth2_model_client_id = @app1.id
   end
 
   describe "GET /messages" do
-    it "should display a paginated list of user's messages" do
-      visit messages_path
-      1.upto(10) do |index|
-        page.should have_content "Message ##{index}"
+    context "when the user has no messages" do
+      it "should inform the user they have no messages" do
+        visit messages_path
+        page.should have_content "You currently have no notifications."
       end
-      page.should_not have_content "Message #11"
-      page.should_not have_content "Other User Message"
-      page.should_not have_content "Other App Message"
-      click_link('2')
-      2.upto(10) do |index|
-        page.should_not have_content "Message ##{index}"
+    end
+    
+    context "when the user has messages" do
+      before do
+        1.upto(14) do |index|
+          @message = Message.new(:subject => "Message ##{index}", :received_at => Time.now - 1.hour, :body => "This is message ##{index}.")
+          @message.user_id = @user.id
+          @message.o_auth2_model_client_id = @app1.id
+          @message.save!
+        end
+        @other_user_message = Message.new(:subject => 'Other User Message', :received_at => Time.now - 1.hour, :body => 'This is a message for a different user.')
+        @other_user_message.user_id = @other_user.id
+        @other_user_message.o_auth2_model_client_id = @app1.id
+        @other_app_message = Message.new(:subject => 'Other App Message', :received_at => Time.now - 1.hour, :body => 'This is a message for a different app.')
+        @other_app_message.user_id = @user.id
+        @other_app_message.o_auth2_model_client_id = @app1.id
       end
-      11.upto(14) do |index|
-        page.should have_content "Message ##{index}"
+      
+      it "should display a paginated list of user's messages" do
+        visit messages_path
+        1.upto(10) do |index|
+          page.should have_content "Message ##{index}"
+        end
+        page.should_not have_content "Message #11"
+        page.should_not have_content "Other User Message"
+        page.should_not have_content "Other App Message"
+        click_link('2')
+        2.upto(10) do |index|
+          page.should_not have_content "Message ##{index}"
+        end
+        11.upto(14) do |index|
+          page.should have_content "Message ##{index}"
+        end
+        click_link('Delete')
+        page.should_not have_content "Message #9"
+        page.should_not have_content "Message #11"
       end
-      click_link('Delete')
-      page.should_not have_content "Message #9"
-      page.should_not have_content "Message #11"
+
+      it "should show the message in detail and allow the user to delete it" do
+        visit messages_path
+        click_link "Message #9"
+        page.should have_content "Message #9"
+        page.should have_content "This is message #9"
+        click_link "Delete"
+        page.should have_content "Message #2"
+        page.should_not have_content "Message #9"
+      end
     end
   end
-  
-  describe "GET /message/:id" do
-    it "should show the message in detail and allow the user to delete it" do
-      visit messages_path
-      click_link "Message #9"
-      page.should have_content "Message #9"
-      page.should have_content "This is message #9"
-      click_link "Delete"
-      page.should have_content "Message #2"
-      page.should_not have_content "Message #9"
-    end
-  end  
 end

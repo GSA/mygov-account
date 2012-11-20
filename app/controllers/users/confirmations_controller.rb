5566b2ecea08a78e9722af94f606300585d01396
Greg Gershman <gregory.gershman@gsa.gov>
2012-11-20 16:54:00 -0500

    Only one welcome experience.

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,6 +1,6 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
   
   def after_confirmation_path_for(resource_name, resource)
-    @segment == "A" ? task_path(resource.tasks.first) : dashboard_path
+    dashboard_path
   end
 end

a5d6416bd2e92d48fad7205603bf065c39e7bd46
Greg Gershman <gregory.gershman@gsa.gov>
2012-11-27 16:01:31 -0500

    [Fixes #39953451] Invitation emails need paragraph breaks

--- config/initializers/01_mygov_config.rb
+++ config/initializers/01_mygov_config.rb
@@ -2,5 +2,5 @@ DEFAULT_FROM_EMAIL = "\"Team MyGov\" <projectmygov@gsa.gov>"
 if Rails.env == "production"
   MYGOV_FORMS_HOME = "https://forms.usa.gov"
 else
-  MYGOV_FORMS_HOME = "http://localhost:3005"
+  MYGOV_FORMS_HOME = "http://localhost:3002"
 end

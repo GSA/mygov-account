3e78da3dab68d37da85163228dfe35dc97d2ee40
Greg Gershman <gregory.gershman@gsa.gov>
2012-12-23 05:50:29 -0500

    Remove unused javascripts; if we need them, we can pull them from github.
     - Remove unused beta signup javascript/partials; clean up views.

--- app/controllers/beta_signups_controller.rb
+++ app/controllers/beta_signups_controller.rb
@@ -3,14 +3,5 @@ class BetaSignupsController < ApplicationController
   
   def create
     beta_signup = BetaSignup.create(params[:beta_signup].merge(:ip_address => request.remote_ip, :referrer => request.referer))
-    if ( beta_signup.errors.messages[:email].nil? )
-      @result = "success"
-    else 
-      @result = beta_signup.errors.messages[:email].first
-    end
-    respond_to do |format|
-        format.html
-        format.json { render :json => { result: @result }  }
-    end
   end
 end

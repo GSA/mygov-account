15eb1b3abd2f5584c2899b290896b1ec736c1ea1
Greg Gershman <gregory.gershman@gsa.gov>
2013-01-09 11:42:08 -0500

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -221,7 +221,6 @@ ActiveRecord::Schema.define(:version => 20121221190323) do
     t.boolean  "is_retired"
   end
 
-  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true

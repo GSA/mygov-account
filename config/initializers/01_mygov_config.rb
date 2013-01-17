3896625a8a8e5c4264a3fedba05890c5195c536a
Greg Gershman <gregory.gershman@gsa.gov>
2013-01-17 06:54:19 -0500

    Prepare config files for open source.

--- config/initializers/01_mygov_config.rb
+++
@@ -1,6 +0,0 @@
-DEFAULT_FROM_EMAIL = "\"Team MyGov\" <projectmygov@gsa.gov>"
-if Rails.env == "production"
-  MYGOV_FORMS_HOME = "https://forms.usa.gov"
-else
-  MYGOV_FORMS_HOME = "http://localhost:3002"
-end

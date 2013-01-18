87ad2c3061b0e3d98c547956849fd20c95e379fe
Greg Gershman <greg@shelrick.com>
2013-01-18 11:43:16 -0500

    [Completes #42749385] Government employee should be automatically approved for beta sign-up

--- app/models/beta_signup.rb
+++ app/models/beta_signup.rb
@@ -3,11 +3,16 @@ class BetaSignup < ActiveRecord::Base
   validates_presence_of :email, {:message => "blank email"}
   validates_uniqueness_of :email, {:message => "duplicate email"}
   validates_email_format_of :email, {:message => "invalid email"}
-  after_update :send_beta_invite
+  before_create :approve_dot_gov_emails
+  after_save :send_beta_invite
   
   private
   
   def send_beta_invite
     (rand(2) == 0 ? UserMailer.beta_invite_a(self.email).deliver : UserMailer.beta_invite_b(self.email).deliver) if is_approved_changed? && is_approved == true
+  end
+  
+  def approve_dot_gov_emails    
+    self.is_approved = true if self.email.end_with?(".gov")
   end
 end
--- spec/models/beta_signup_spec.rb
+++ spec/models/beta_signup_spec.rb
@@ -1,10 +1,42 @@
 require 'spec_helper'
 
 describe BetaSignup do
+  before do
+    @valid_attributes = {
+      :email => 'joe@citizen.org'
+    }
+    ActionMailer::Base.deliveries = []
+  end
+  
   it { should validate_presence_of(:email).with_message("blank email") }
   
-  it "should create a new record given valid attributes" do
-    BetaSignup.create!(:email => 'joe@citizen.org')
+  it "should create a new record given valid attributes, but not be approved, and not send an email" do
+    beta_signup = BetaSignup.create!(@valid_attributes)
     should validate_uniqueness_of(:email).with_message(/duplicate email/)
+    beta_signup.is_approved.should == false
+    ActionMailer::Base.deliveries.should be_empty
   end
+  
+  it "should send an email after update if the sign up has been approved" do
+    beta_signup = BetaSignup.create!(@valid_attributes)
+    beta_signup.is_approved = true
+    beta_signup.save!
+    ActionMailer::Base.deliveries.size.should == 1    
+    email = ActionMailer::Base.deliveries.first
+    email.from.should == ["projectmygov@gsa.gov"]
+    email.to.should == [beta_signup.email]
+    email.subject.should =~ /MyGov/
+    email.subject.should =~ /Beta/
+    email.body.should =~ /After months of research/
+  end
+  
+  context "when signing up with a .gov email address" do
+    it "should automatically approve a signup if it's a .gov email address and send an email" do
+      beta_signup = BetaSignup.create!(:email => 'test@america.gov')
+      beta_signup.is_approved.should be_true
+      ActionMailer::Base.deliveries.size.should == 1
+      email = ActionMailer::Base.deliveries.first
+      email.body.should =~ /After months of research/
+    end
+  end  
 end

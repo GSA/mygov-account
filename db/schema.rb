44c1515a2252a7d18a855058c6fcb67cdf672d82
Greg Gershman <greg@shelrick.com>
2013-05-02 18:10:21 -0400

    [Completes #49164647] Store session information in the database, not in cookies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130502165245) do
+ActiveRecord::Schema.define(:version => 20130502192418) do
 
   create_table "app_oauth_scopes", :force => true do |t|
     t.integer  "app_id"
@@ -160,6 +160,16 @@ ActiveRecord::Schema.define(:version => 20130502165245) do
   end
 
   add_index "related_urls", ["url", "occurence_count"], :name => "index_related_urls_on_url_and_occurence_count"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "submitted_forms", :force => true do |t|
     t.integer  "user_id"

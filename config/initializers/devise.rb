14431d09eecc997d4a44dfc32941645d774f2164
Greg Gershman <greg@shelrick.com>
2013-05-17 19:23:08 -0400

    Add PayPal back into production third-party identity providers.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,7 @@ if Rails.env != 'development'
     require 'devise/orm/active_record'
     require 'openid/store/filesystem'
     config.omniauth :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :require => 'omniauth-openid', :preferred_auth_policies => PAPE_FIELDS[:preferred_auth_policies], :max_auth_age => PAPE_FIELDS[:max_auth_age], :required => ["http://axschema.org/contact/email"], :optional => []
+    config.omniauth :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'paypal', :identifier => 'https://www.paypal.com/webapps/auth/server', :require => 'omniauth-openid', :preferred_auth_policies => PAPE_FIELDS[:preferred_auth_policies], :max_auth_age => PAPE_FIELDS[:max_auth_age], :required => ["http://axschema.org/contact/email"], :optional => []
     config.omniauth :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'verisign', :identifier => 'https://pip.verisignlabs.com/user/DIRECTED_IDENTITY_USER/yadisxrds', :require => 'omniauth-openid', :require => 'omniauth-openid', :preferred_auth_policies => PAPE_FIELDS[:preferred_auth_policies], :max_auth_age => PAPE_FIELDS[:max_auth_age], :required => ["email"], :optional => []
     config.reconfirmable = true
     config.mailer_sender = DEFAULT_FROM_EMAIL

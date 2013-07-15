3f4c638c3bfd68a5ee7a7a5edaf147ef818b8799
Greg Gershman <greg@shelrick.com>
2013-07-14 22:33:55 -0400

    Fix RemoveProviderFromUsersMigration to iterate over users.

--- db/migrate/20130708211943_remove_provider_from_users.rb
+++ db/migrate/20130708211943_remove_provider_from_users.rb
@@ -1,16 +1,22 @@
 class RemoveProviderFromUsers < ActiveRecord::Migration
   def up
-  	User.all do |user|
-  		user.authentications << Authentication.new(:uid => user.uid, :provider => user.provider) if Authentication.where(:uid => user.uid, :provider => user.provider).empty?
-  		user.update_attributes(:uid => SecureRandom.uuid) if user.uid.blank? or user.uid.include? 'http'
-  	end
+    User.all.each do |user|
+      provider = 'google' if user.uid =~/google/
+      provider = 'verisign' if user.uid =~ /verisign/
+      provider = 'paypal' if user.uid =~ /paypal/
+      if provider and ['google', 'paypal', 'verisign'].include?(provider) and Authentication.find_by_uid_and_provider(user.uid, provider).nil?
+        user.authentications << Authentication.new(:uid => user.uid, :provider => provider)
+      end
+      user.uid = SecureRandom.uuid if user.uid.blank? or user.uid.include? 'http'
+      user.save
+    end
   	remove_column :users, :provider
   end
 
   def down
   	add_column :users, :provider, :string
 
-  	User.all do |user|
+  	User.all.each do |user|
   		user.update_attributes(:uid => user.authentications.first.uid, :provider => user.authentications.first.provider)
   	end
   end

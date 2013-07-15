493455a71206692012b5bd64a0ec4f87d99e3ef3
Greg Gershman <greg@shelrick.com>
2013-07-14 21:44:50 -0400

    Alter 'remove_provider_from_users' migration to add authentications via a scope off users.

--- db/migrate/20130708211943_remove_provider_from_users.rb
+++ db/migrate/20130708211943_remove_provider_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveProviderFromUsers < ActiveRecord::Migration
   def up
   	User.all do |user|
-  		Authenication.create(:user => user, :uid => user.uid, :provider => user.provider) if Authentication.where(:uid => user.uid, :provider => user.provider).empty?
+  		user.authentications << Authentication.new(:uid => user.uid, :provider => user.provider) if Authentication.where(:uid => user.uid, :provider => user.provider).empty?
   		user.update_attributes(:uid => SecureRandom.uuid) if user.uid.blank? or user.uid.include? 'http'
   	end
   	remove_column :users, :provider

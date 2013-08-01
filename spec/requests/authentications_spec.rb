dc534d2ccc5b1ef3b58cce9d1ce3e889c28697bf
Amos Stone <amos@amosarts.com>
2013-08-01 18:24:28 -0400

    Adds additional authentication providers to view.
    Removes providers from 'add' list which user already has.
    
    Adds tests for user add/remove of authentications
    
    Updates tests to utilize rspec2 syntax

--- spec/requests/authentications_spec.rb
+++ spec/requests/authentications_spec.rb
@@ -9,16 +9,45 @@ describe "Authentications" do
   end
   
   describe "adding a new authentication" do
-    context "when the user does not have any additional authentication providers" do
-      it "should allow the user to connect their MAX.gov account" do
+    context "when the user does not have a MAX authentication" do
+      it 'allows the user to connect their Max.gov account' do
         visit root_path
         click_link 'Settings'
         click_link 'Authentication providers'
         click_link 'Add an authentication provider to your account'
         click_link 'Connect your MAX.gov account'
-        page.should have_content 'max.gov'
+        expect(page).to have_content 'Max.Gov'
       end
     end
+
+    context "when the user already has a MAX authentication" do
+      before do
+        @user.authentications << Authentication.create!(:uid => '12345', :provider => 'max.gov')
+      end
+
+      it 'does not allow the user to connect their Max.gov account' do
+        visit root_path
+        click_link 'Settings'
+        click_link 'Authentication providers'
+        click_link 'Add an authentication provider to your account'
+        expect(page).to_not have_content 'Max.Gov'
+      end
+    end
+
+    context "when the user does not have a google authentication" do
+      before do
+        @user.authentications.each {|auth| auth.destroy}
+      end
+
+      it 'allows the user to connect to google' do
+        visit root_path
+        click_link 'Settings'
+        click_link 'Authentication providers'
+        click_link 'Add an authentication provider to your account'
+        expect(page).to have_content 'Google'
+      end
+    end
+
   end
   
   describe "deleting an authentication" do
@@ -27,18 +56,18 @@ describe "Authentications" do
         @user.authentications.create(:provider => "max.gov", :uid => 'joe.citizen@usa.gov')
       end
       
-      it "should allow the user to delete their authentication, which should disable login with that provider" do
+      it 'allows the user to delete their authentication which disables login with that provider' do
         visit root_path
         click_link 'Settings'
         click_link 'Authentication providers'
-        page.should have_content 'max.gov'
+        page.should have_content 'Max.Gov'
         click_link 'Delete'
-        page.should_not have_content 'max.gov'
+        page.should_not have_content 'Max.Gov'
         click_link 'Sign out'
         visit sign_in_path
         click_link 'More sign in options'
         click_link 'Sign in with MAX.gov'
-        page.should have_content "I'm sorry, we don't have an account associated with your MAX.gov account."
+        expect(page).to have_content "I'm sorry, we don't have an account associated with your MAX.gov account."
       end
     end
   end

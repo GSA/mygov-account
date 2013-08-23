5860655db5de4a70c03b7499a3ac947f74ad9c39
Greg Gershman <greg@shelrick.com>
2013-08-23 09:46:50 -0400

    [Completes #52331399] Users can update their email address - Clean up view, add flash notice, display unconfirmed email instead of confirmed email.

--- spec/requests/settings_spec.rb
+++ spec/requests/settings_spec.rb
@@ -20,7 +20,7 @@ describe "SettingsPage" do
     end
   end
 
-  describe "GET /user/edit.:id" do
+  describe "GET /user/edit" do
     context "when the user is logged in" do
       before do
         create_logged_in_user(@user)
@@ -34,28 +34,33 @@ describe "SettingsPage" do
 
       it "should let the user change their email address" do
         visit edit_user_registration_path(@user)
-        fill_in('Email', :with => 'joseph@citizen.org')
+        fill_in('Email', :with => 'jack@citizen.org')
         fill_in('Current password', :with => 'Password1')
         click_button('Update')
         page.should have_no_content('your account hasn\'t been approved yet')
-
+        expect(page).to have_content('You updated your account successfully, but we need to verify your new email address.')
+        page.body.should =~ /jack/
+        page.body.should_not =~ /joe/
         @user.reload
-        @user.unconfirmed_email.should == 'joseph@citizen.org'
+        @user.unconfirmed_email.should == 'jack@citizen.org'
+        email = ActionMailer::Base.deliveries.last
+        email.to.should == ['jack@citizen.org']
+        email.from.should == ["projectmyusa@gsa.gov"]
+        expect(email.body).to include('jack@citizen.org')
+        expect(email.body).not_to include('joe@citizen.org')
       end
 
       context "when the user changes their email address" do
         it "should change their beta invite address once they have confirmed the new one" do
           visit edit_user_registration_path(@user)
-          fill_in('Email', :with => 'joseph@citizen.org')
+          fill_in('Email', :with => 'jack@citizen.org')
           fill_in('Current password', :with => 'Password1')
           click_button('Update')
-          BetaSignup.where(:email => 'joseph@citizen.org').count.should == 0
+          BetaSignup.where(:email => 'jack@citizen.org').count.should == 0
           BetaSignup.where(:email => 'joe@citizen.org').count.should == 1
-
           @user.reload
           @user.confirm!
-
-          BetaSignup.where(:email => 'joseph@citizen.org').count.should == 1
+          BetaSignup.where(:email => 'jack@citizen.org').count.should == 1
           BetaSignup.where(:email => 'joe@citizen.org').count.should == 0
         end
       end

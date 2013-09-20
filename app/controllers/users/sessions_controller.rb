3d04d862711cbc9eca76cdec464ed8e04c8876d0
frumioj <john@jkemp.net>
2013-09-20 11:26:29 -0400

    return a JSON structure with user key name for use in encrypted profile

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,17 @@
 class Users::SessionsController < Devise::SessionsController
+
+  ## @@NOTE: overriding the Devise create method to add the custom
+  ## render which breaks the redirect into two pieces
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+    render :json => { :user_key_storage_name => @user.key_storage_name, :after_signin_path => after_sign_in_path_for(resource) }.to_json() 
+
+    # previously, following resource line was returned
+    # respond_with resource, :location => after_sign_in_path_for(resource)
+  end
   
   def more_sign_in_options
   end

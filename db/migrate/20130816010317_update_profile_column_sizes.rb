5d881361e1303a1a4c9f52f1de9f59cb6a6fd1dc
frumioj <john@jkemp.net>
2013-09-20 21:37:52 -0400

    changed column sizes

--- db/migrate/20130816010317_update_profile_column_sizes.rb
+++ db/migrate/20130816010317_update_profile_column_sizes.rb
@@ -1,8 +1,7 @@
 class UpdateProfileColumnSizes < ActiveRecord::Migration
   def change
-    change_column :profiles, :zip, :string, :limit => 25
-    change_column :profiles, :state, :string, :limit => 25
-    change_column :profiles, :phone, :string, :limit => 50
-    change_column :profiles, :mobile, :string, :limit => 50
+    change_column :profiles, :zip, :string, :limit => 50
+    change_column :profiles, :phone, :string, :limit => 100
+    change_column :profiles, :mobile, :string, :limit => 100
   end
 end

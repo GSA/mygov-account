3ba813b3edae6f88c9708c9ae46a1dae2cc54546
Raphael Villas <raphael.villas@gmail.com>
2013-10-21 12:45:15 -0500

    *** WIP: Implement initial layout working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,39 +1,39 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :developer, :privacy_policy, :terms_of_service, :about, :xrds]
   before_filter :assign_user, :except => [:index, :privacy_policy, :developer, :terms_of_service, :xrds]
-  
+
   def index
     if current_user
-      redirect_to :dashboard 
+      redirect_to :dashboard
     else
       @beta_signup = BetaSignup.new
-      render :layout => 'signup'
+      render :layout => 'signup_new'
     end
   end
-  
+
   def dashboard
     @today = Date.current
     @uncompleted_tasks = @user.tasks.uncompleted.order('created_at DESC')
   end
-  
+
   def developer
   end
 
   def discovery
   end
-  
+
   def privacy_policy
   end
-  
+
   def terms_of_service
   end
-  
+
   def about
   end
-  
+
   def pra
   end
-    
+
   def xrds
     response.content_type = "application/xrds+xml"
   end

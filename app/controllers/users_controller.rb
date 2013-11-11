311988e6fcbd90b260f3de21c5f4a2a6b3d2e9cf
mikefab <mike@mobomo.com>
2013-11-11 04:32:24 -0500

    User can change password from settings page within MyUSA Account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,35 @@ class UsersController < ApplicationController
     sign_out :user
     @user.destroy
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def edit_password
+    @user = current_user
+  end
+
+  def update_password
+    @user = User.find(current_user.id)
+    if @user.update_attributes(user_params)
+      # Sign in the user by passing validation in case his password changed
+      sign_in @user, :bypass => true
+      flash[:notice] = "Your password was sucessfully updated."
+      redirect_to settings_path
+    else
+      @user
+      render 'edit_password'
+    end
+  end
+  
+  private
+
+  def user_params
+    # NOTE: Using `strong_parameters` gem
+    params.required(:user).permit(:password, :password_confirmation)
+  end
+
+
+
 end

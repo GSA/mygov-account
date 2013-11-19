aa117f2eb60c8bb9e041ffa26de1168c0388a416
Amos Stone <amos@amosarts.com>
2013-11-19 16:01:02 -0500

    Updates resque initializer to ignore yml file in test environment

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 # Locks down /resque with HTTP basic auth
 # 	if/when an admin role is reinstated, kill this and update routes.rb
-unless Rails.env == 'development'
+unless Rails.env == 'development' or Rails.env == 'test'
 	resque_config_file = File.join(Rails.root,'config','resque.yml')
 	raise "#{resque_config_file} is missing!" unless File.exists? resque_config_file
 	resque_config = YAML.load_file(resque_config_file).symbolize_keys

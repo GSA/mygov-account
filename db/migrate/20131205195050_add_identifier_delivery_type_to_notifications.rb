fc13c9b08e1f9806d8c12d791a8a83830ffef99a
Raphael Villas <raphael.villas@gmail.com>
2013-12-06 08:32:06 -0500

    ** Update migration **

--- db/migrate/20131205195050_add_identifier_delivery_type_to_notifications.rb
+++ db/migrate/20131205195050_add_identifier_delivery_type_to_notifications.rb
@@ -1,6 +1,5 @@
 class AddIdentifierDeliveryTypeToNotifications < ActiveRecord::Migration
   def change
     add_column :notifications, :identifier, :string
-    add_column :notifications, :delivery_type, :string
   end
 end

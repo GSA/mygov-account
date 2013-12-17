65b4817abf7b90e91d2bd74e79290dca453034fb
Raphael Villas <raphael.villas@gmail.com>
2013-12-17 01:06:36 -0500

    Updated NotificationText worker

--- app/workers/notification_text.rb
+++ app/workers/notification_text.rb
@@ -4,16 +4,16 @@ require 'twilio-ruby'
 
 class NotificationText
 
-  account_sid = ENV['TWILIO_ACCOUNT_SID']
-  auth_token = ENV['TWILIO_AUTH_TOKEN']
-  @client = Twilio::REST::Client.new account_sid, auth_token
-
   @queue = :sms
   def self.perform(notification_id)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new account_sid, auth_token
+
     notification = Notification.find_by_id(notification_id)
 
 
-    @client.account.messages.create(
+    client.account.messages.create(
       :from => ENV['TWILIO_FROM_NUMBER'],
       :to => notification.user.profile.mobile_for_twilio,
       :body => "#{notification.subject} -- #{notification.body if notification.body}"

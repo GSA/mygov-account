c8645bfcb37eabd4cda111e52eb3a88fd41ce67d
Adam Frazin <adam@mobomo.com>
2013-12-19 20:18:15 -0500

    [#61502574] Small code refactor

--- app/models/uri_validator.rb
+++ app/models/uri_validator.rb
@@ -2,9 +2,10 @@ class UriValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     begin
       uri = URI.parse(value)
-      record.errors.add(attribute, (options[:message] || I18n.t('invalid_uri'))) unless uri.absolute?
+      message = options[:message] || I18n.t('invalid_uri')
+      record.errors.add(attribute, message) unless uri.absolute?
     rescue URI::InvalidURIError
-      record.errors.add(attribute, (options[:message] || I18n.t('invalid_uri')))
+      record.errors.add(attribute, message)
     end
   end
 end

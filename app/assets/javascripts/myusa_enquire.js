3b9df7cf1fb66c9fda0151c84a3ded9422fa26df
Raphael Villas <raphael.villas@gmail.com>
2014-01-13 12:57:21 -0600

    *** Breaking stuff ***

---
+++ app/assets/javascripts/myusa_enquire.js
@@ -0,0 +1,31 @@
+$(function() {
+
+  enquire.register("screen and (max-width:240px)", {
+
+    // OPTIONAL
+    // If supplied, triggered when a media query matches.
+    match : function() {
+      $("input#beta_signup_email").val('Email address');
+    },
+
+    // OPTIONAL
+    // If supplied, triggered when the media query transitions
+    // *from a matched state to an unmatched state*.
+    unmatch : function() {},
+
+    // OPTIONAL
+    // If supplied, triggered once, when the handler is registered.
+    setup : function() {},
+
+    // OPTIONAL, defaults to false
+    // If set to true, defers execution of the setup function
+    // until the first time the media query is matched
+    deferSetup : true,
+
+    // OPTIONAL
+    // If supplied, triggered when handler is unregistered.
+    // Place cleanup code here
+    destroy : function() {}
+
+  })
+});

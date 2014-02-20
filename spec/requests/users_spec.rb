529881fde38e4ea1f4c7f6622629182c855edd25
Adam Frazin <adam@mobomo.com>
2014-02-20 14:55:54 -0500

    [Completes #66177374] Remove "Remember me"

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -63,6 +63,7 @@ describe "Users" do
         page.should have_content "Sign in"
         click_link "Sign in"
         current_path.should eq sign_in_path
+        page.should_not have_content "Remember me"
         
         visit root_path
         page.should have_content "Already using MyUSA?"

8e6b042a2e00a3015c27ac08f3f2b18605bbf21e
Ryan Rusnak <ryandrusnak@gmail.com>
2014-04-04 13:06:09 -0400

    [Completes #68884384] User views beta message in a popup view
    Tweaks to Beta message and to message placement

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,29 +9,34 @@ describe "Users" do
         page.should have_content "Sign in"
         click_link "Sign in"
         current_path.should == sign_in_path
-        
+
         visit root_path
         page.should have_content "Already using MyUSA?"
         page.should have_content "Sign in"
         click_link "Sign in"
         current_path.should == sign_in_path
       end
-      
+
       it "should link to third party sign ins" do
         visit sign_in_path
         page.should have_link 'Sign in with Google'
         page.should have_link 'Sign in with VeriSign'
       end
-      
+
       it "should not have a sign-in link on the sign-in page" do
         visit sign_in_path
         page.should_not have_content "Already using MyUSA?"
       end
+      
+      it "should not have a beta modal" do
+        visit sign_in_path
+        page.should_not have_content "MyUSA is currently in limited Beta use. Only Beta testers are currently able to sign up."
+      end
     end
-    
+
     context "when a user is signed in" do
       before {create_confirmed_user; login(@user)}
-      
+
       it "should not ask the user to sign in" do
         visit dashboard_path
         page.should_not have_content "Already using MyUSA?"
@@ -39,14 +44,14 @@ describe "Users" do
       end
     end
   end
-  
+
   describe "sign up process" do
     it "should provide links to third party sign in services" do
       visit sign_up_path
       page.should have_link 'Sign up with Google'
       page.should have_link 'Sign up with VeriSign'
     end
-    
+
     context "when a user is not in the beta signup list" do
       it "should not let the user create an account" do
         visit sign_up_path
@@ -55,7 +60,7 @@ describe "Users" do
         click_button 'Sign up'
         page.should have_content "I'm sorry, your account hasn't been approved yet."
       end
-      
+
       it "should not let the user create an approved beta signup record by passing in is_approved to the post request" do
         test_email = 'shady@citizen.org'
         begin
@@ -65,7 +70,7 @@ describe "Users" do
         end
         BetaSignup.where(email: test_email, is_approved: true).first.should eq nil
       end
-      
+
       it "should not let the user create an approved beta signup record by manipulating the email address" do
         test_email = "shady@citizen.org"
         visit root_path
@@ -75,10 +80,10 @@ describe "Users" do
         BetaSignup.find_by_email(test_email + "&is_approved=1").should be_nil
       end
     end
-    
+
     context "when a user is in the beta signup list" do
       before { BetaSignup.create!(:email => 'joe@citizen.org') }
-      
+
       context "when a user has not been approved" do
         it "should not let the user create an account" do
           visit sign_up_path
@@ -88,7 +93,7 @@ describe "Users" do
           page.should have_content "I'm sorry, your account hasn't been approved yet."
         end
       end
-    
+
       context "when a user has been approved" do
         before { BetaSignup.find_by_email('joe@citizen.org').update_attribute(:is_approved, true) }
 
@@ -101,7 +106,7 @@ describe "Users" do
               page.should have_content "Terms of service must be accepted"
             end
           end
-          
+
           context "when the user doesn't fill in everything else" do
             it "should display error messages for all the missing bits of information" do
               visit sign_up_path
@@ -111,13 +116,13 @@ describe "Users" do
             end
           end
         end
-        
+
         context "when no user exists with the supplied email address" do
           before do
             user = User.find_by_email('joe@citizen.org')
             user.destroy if user
           end
-          
+
           it "should let the user create an account" do
             visit sign_up_path
             fill_in_email_and_password
@@ -127,7 +132,7 @@ describe "Users" do
             ActionMailer::Base.deliveries.last.to.should == ['joe@citizen.org']
             ActionMailer::Base.deliveries.last.from.should == ["projectmyusa@gsa.gov"]
           end
-          
+
           context "when the user submits a password that has less than 8 characters" do
             it "should not create the user account" do
               visit sign_up_path
@@ -137,7 +142,7 @@ describe "Users" do
               page.should have_content 'Password is too short (minimum is 8 characters)'
             end
           end
-          
+
           context "when the user submits a password that isn't sufficiently strong" do
             it "should not create the user account" do
               visit sign_up_path
@@ -147,7 +152,7 @@ describe "Users" do
               page.should have_content 'must include at least one lower case letter, one upper case letter and one digit.'
             end
           end
-          
+
           context "when the user submits a password with special characters" do
             it "should create an account" do
               visit sign_up_path
@@ -156,16 +161,16 @@ describe "Users" do
               click_button 'Sign up'
               page.should have_content 'Thank you for signing up'
             end
-          end 
+          end
         end
-        
+
         context "when a third-party user exists with the same email but a different uid and provider" do
           before do
             user = User.new(:email => 'joe@citizen.org', :password => 'Password1')
             user.authentications.new(:provider => 'other_provider', :uid => 'joe@citizen.org')
             user.save!
           end
-          
+
           it "should not allow a new user to be created or login with that email address" do
             visit sign_up_path
             fill_in_email_and_password
@@ -174,16 +179,16 @@ describe "Users" do
             page.should have_content 'Email has already been taken'
           end
         end
-        
+
         context "when a local user exists with the same email" do
           before { User.create(:email => 'joe@citizen.org', :password => 'Password1') }
-          
+
           it "should not let someone sign in with a third party service that identifies the user with the same email" do
             visit sign_in_path
             click_link 'Sign in with Google'
             page.should have_content 'We already have an account with that email. Make sure login with the service you used to create the account.'
           end
-        end 
+        end
 
         it "should set the user's name" do
           visit sign_up_path
@@ -196,7 +201,7 @@ describe "Users" do
           ActionMailer::Base.deliveries.last.to.should == ['joe@citizen.org']
           ActionMailer::Base.deliveries.last.from.should == ["projectmyusa@gsa.gov"]
           User.find_by_email('joe@citizen.org').profile.name.should == 'Joe Citizen'
-        end    
+        end
       end
     end
   end
@@ -213,10 +218,10 @@ describe "Users" do
       ActionMailer::Base.deliveries.last.to.should == ['joe@citizen.org']
       ActionMailer::Base.deliveries.last.subject.should == 'Unlock Instructions'
     end
-    
+
     context "when the user has connected their MAX.gov account" do
       before {@user.authentications.create(:provider => "max.gov", :uid => 'joe.citizen@usa.gov')}
-      
+
       it "should allow the user to log in using their MAX.gov account" do
         visit sign_in_path
         click_link 'More sign in options'
@@ -225,7 +230,7 @@ describe "Users" do
       end
     end
   end
-    
+
   describe "sign out process" do
     before {create_confirmed_user_with_profile; login(@user)}
 
@@ -239,7 +244,7 @@ describe "Users" do
 
   describe "change your name" do
     before {create_confirmed_user_with_profile; login(@user)}
-    
+
     it "should change the user's name when first or last name changes" do
       visit edit_profile_path
       fill_in 'First name', :with => 'Jane'
@@ -249,18 +254,18 @@ describe "Users" do
       page.should have_content "Jane Citizen"
     end
   end
-  
+
   describe "change your password" do
     before {create_confirmed_user}
-    
+
     it "changes the user's password and sends notification and confirmation emails" do
       visit sign_in_path
       click_link "Forgot your password?"
       fill_in 'Email', :with => 'joe@citizen.org'
       click_button "Email password reset instructions"
-      
+
       expect(ActionMailer::Base.deliveries.last.subject).to eq('Reset password instructions')
-      
+
       @user.reload
 
       expect(@user.reset_password_token.blank?).to be false
@@ -273,10 +278,10 @@ describe "Users" do
       expect(ActionMailer::Base.deliveries.last.subject).to eq('Your MyUSA password has been changed')
     end
   end
-  
+
   describe "login, confirmation, and unlock messages" do
     before { create_confirmed_user }
-    
+
     it "yields the same message irregardless of the email's existence in the db for login attempts" do
       visit sign_in_path
       fill_in 'user_email', with: 'joe@citizen.org'
@@ -285,7 +290,7 @@ describe "Users" do
 
       fill_in 'user_email', with: 'joe_schmoe@citizen.org'
       click_button "Sign in"
-      
+
       expect(find('div.alert-box').text.squish).to eq alert_message
     end
 
@@ -297,7 +302,7 @@ describe "Users" do
 
       fill_in 'user_email', with: 'joe_schmoe@citizen.org'
       click_button "Email password reset instructions"
-      
+
       expect(find('div.alert-box').text.squish).to eq alert_message
     end
 
@@ -309,7 +314,7 @@ describe "Users" do
 
       fill_in 'user_email', with: 'joe_schmoe@citizen.org'
       click_button "Send"
-      
+
       expect(find('div.alert-box').text.squish).to eq alert_message
     end
 
@@ -321,7 +326,7 @@ describe "Users" do
 
       fill_in 'user_email', with: 'joe_schmoe@citizen.org'
       click_button "Send"
-      
+
       expect(find('div.alert-box').text.squish).to eq alert_message
     end
   end

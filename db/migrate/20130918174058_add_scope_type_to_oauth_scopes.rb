a997f22b82aa829c2736165db58bbb76ecd2956a
mikefab <mikefabrikant@gmail.com>
2014-04-09 18:48:42 -0300

    [Completes #68371912] Migration for new grouping of scopes needs to include grandfathered scopes by updating their :scope_type attributes.

--- db/migrate/20130918174058_add_scope_type_to_oauth_scopes.rb
+++ db/migrate/20130918174058_add_scope_type_to_oauth_scopes.rb
@@ -1,5 +1,7 @@
 class AddScopeTypeToOauthScopes < ActiveRecord::Migration
   def change
     add_column :oauth_scopes, :scope_type, :string, :limit => 20
+    OauthScope.all.each{|os| scope_type = (os.scope_name == "verify_credentials" ? 'app' : 'user'); os.update_attributes!(scope_type: scope_type) }
+    say "#{OauthScope.count} scope records updated."
   end
 end

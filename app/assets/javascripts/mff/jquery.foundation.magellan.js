93c7ff1713f2c02fb675962f891f70e31f2c9f45
Ryan Rusnak <ryandrusnak@gmail.com>
2014-04-15 12:54:28 -0400

    [Completes #69482086] ScrollTo Javascript error on sign up page

--- app/assets/javascripts/mff/jquery.foundation.magellan.js
+++ app/assets/javascripts/mff/jquery.foundation.magellan.js
@@ -12,26 +12,23 @@
   'use strict';
 
   $.fn.foundationMagellan = function(options) {
-    var $window = $(window),
-  		$document = $(document),
-  		$fixedMagellan = $('[data-magellan-expedition=fixed]'),
-    	defaults = {
-	      threshold: ($fixedMagellan.length) ? $fixedMagellan.outerHeight(true) : 0,
-	      activeClass: 'active'
-	    },
-		options = $.extend({}, defaults, options);
-	
+    var defaults = {
+      threshold: 25,
+      activeClass: 'active'
+    },
+
+    options = $.extend({}, defaults, options);
+
     // Indicate we have arrived at a destination
-    $document.on('magellan.arrival', '[data-magellan-arrival]', function(e) {
-		var $destination = $(this),
-      		$expedition = $destination.closest('[data-magellan-expedition]'),
-			activeClass = $expedition.attr('data-magellan-active-class') || options.activeClass;
-      $destination
+    $(document).on('magellan.arrival', '[data-magellan-arrival]', function(e) {
+      var $expedition = $(this).closest('[data-magellan-expedition]'),
+          activeClass = $expedition.attr('data-magellan-active-class') || options.activeClass;
+      $(this)
         .closest('[data-magellan-expedition]')
         .find('[data-magellan-arrival]')
         .not(this)
         .removeClass(activeClass);
-      $destination.addClass(activeClass);
+      $(this).addClass(activeClass);
     });
 
     // Set starting point as the current destination
@@ -40,20 +37,21 @@
       .addClass($expedition.attr('data-magellan-active-class') || options.activeClass);
 
     // Update fixed position
-    $fixedMagellan.on('magellan.update-position', function(){
+    $('[data-magellan-expedition=fixed]').on('magellan.update-position', function(){
       var $el = $(this);
       $el.data("magellan-fixed-position","");
       $el.data("magellan-top-offset", "");
-    })
-    .trigger('magellan.update-position');
-
-    $window.on('resize.magellan', function() {
-      $fixedMagellan.trigger('magellan.update-position');
     });
-    
-    $window.on('scroll.magellan', function() {
-      var windowScrollTop = $window.scrollTop();
-      $fixedMagellan.each(function() {
+
+    $('[data-magellan-expedition=fixed]').trigger('magellan.update-position');
+
+    $(window).on('resize.magellan', function() {
+      $('[data-magellan-expedition=fixed]').trigger('magellan.update-position');
+    });
+
+    $(window).on('scroll.magellan', function() {
+      var windowScrollTop = $(window).scrollTop();
+      $('[data-magellan-expedition=fixed]').each(function() {
         var $expedition = $(this);
         if ($expedition.data("magellan-top-offset") === "") {
           $expedition.data("magellan-top-offset", $expedition.offset().top);
@@ -71,21 +69,15 @@
     });
 
     // Determine when a destination has been reached, ah0y!
-    var $lastDestination = $('[data-magellan-destination]:last');
-    $window.on('scroll.magellan', function(e){
-	  var windowScrollTop = $window.scrollTop(),
-		  scrolltopPlusHeight = windowScrollTop+$window.outerHeight(true),
-		  lastDestinationTop = Math.ceil($lastDestination.offset().top);
+    $(window).on('scroll.magellan', function(e){
+      var windowScrollTop = $(window).scrollTop();
       $('[data-magellan-destination]').each(function(){
         var $destination = $(this),
             destination_name = $destination.attr('data-magellan-destination'),
             topOffset = $destination.offset().top - windowScrollTop;
         if (topOffset <= options.threshold) {
-          $('[data-magellan-arrival=' + destination_name + ']').trigger('magellan.arrival');
-        }
-        // In large screens we may hit the bottom of the page and dont reach the top of the last magellan-destination, so lets force it
-        if (scrolltopPlusHeight >= $document.outerHeight(true) && lastDestinationTop > windowScrollTop && lastDestinationTop < scrolltopPlusHeight) {
-        	 $('[data-magellan-arrival]:last').trigger('magellan.arrival');
+          $('[data-magellan-arrival=' + destination_name + ']')
+            .trigger('magellan.arrival');
         }
       });
     });
